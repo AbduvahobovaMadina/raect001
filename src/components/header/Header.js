eda26bd4691f1cb548111fa3404c64da55abcf4b
AbduvahobovaMadina <mailto:madinaabduvahobova565@gmail.com>
2024-07-30 23:50:13 +0500

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { IoSearchOutline } from "react-icons/io5";
+
+
+
+function Header(){
+    return (
+        <div>
+            <h2>Header</h2>
+            <IoSearchOutline />
+        </div>
+    )
+}
+
+
+export default Header
